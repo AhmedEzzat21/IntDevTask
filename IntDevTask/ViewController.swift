b5b4145f0b6debf5c321b34dae932221b046f354
AhmedEzzat21 <a7med.ibn.3zzat@gmail.com>
2018-09-22 18:03:58 +0200

    Initial Commit

---
+++ IntDevTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntDevTask
+//
+//  Created by Ahmed Ezzat on 9/22/18.
+//  Copyright © 2018 Ahmed Ezzat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
